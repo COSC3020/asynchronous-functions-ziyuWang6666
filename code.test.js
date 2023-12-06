1ce9cf5d3044f80a8de029b72f41111547cb30a9
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-05 21:20:43 -0700

    Update code.test.js
    change content

--- code.test.js
+++ code.test.js
@@ -1,28 +1,26 @@
-
 const fs = require('fs');
 const assert = require('assert');
+const { resolve } = require('path');
 eval(fs.readFileSync('code.js')+'');
 
-async function runTests() {
-    // normal case
-    const arr1 = [3, 5, 9, 3, 4, 6, 7, 2, 1, 8, 3, 3, 5, 2, 3, 9];
-    const key1 = 3;
-    const res1 = await new Promise((resolve) => nmPTP(arr1, key1, resolve));
-    assert.strictEqual(res1, 5);
+// normal case
+const arr1 = [3, 5, 9, 3, 4, 6, 7, 2, 1, 8, 3, 3, 5, 2, 3, 9];
+const key1 = 3;
+nmPTP(arr1, key1, function(result) {
+    assert(result == 5);
+});
 
-    // empty array
-    const arr2 = [];
-    const key2 = 3;
-    const res2 = await new Promise((resolve) => nmPTP(arr2, key2, resolve));
-    assert.strictEqual(res2, 0);
 
-    // 0 occurrences
-    const arr3 = [-1, 10, 20, 98, 2, 30, 8, 7, 4];
-    const key3 = 3;
-    const res3 = await new Promise((resolve) => nmPTP(arr3, key3, resolve));
-    assert.strictEqual(res3, 0);
+// empty array
+const arr2 = [];
+const key2 = 3;
+nmPTP(arr2, key2, function(result) {
+    assert(result == 0);
+});
 
-    // console.log('All tests passed.');
-}
-
-runTests();
+// 0 occurrences
+const arr3 = [-1, 10, 20, 98, 2, 30, 8, 7, 4];
+const key3 = 3;
+nmPTP(arr3, key3, function(result) {
+    assert(result == 0);
+});
