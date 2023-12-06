ac8bfbb481c438c7f516dc5330cb4c6f7673dd9d
ziyuWang6666 <144853480+ziyuWang6666@users.noreply.github.com>
2023-12-05 21:01:01 -0700

    Create code.js
    file name change

---
+++ code.js
@@ -0,0 +1,32 @@
+function getCounts(arr, key) {
+    return new Promise((resolve)=> {
+        if(key=== null) {
+            resolve(0);
+        } else {
+            let m = 0;
+            for( var i = 0; i < arr.length; i++) {
+                if(arr[i] == key) {
+                    m++;
+                }
+            }
+            resolve(m);
+        }
+    })
+    
+}
+
+const async = require('async');
+function nmPTP(arr, key, done) {
+    var results = 0, len = arr.length;
+    const size = Math.ceil(len/4);
+
+    (async () => {
+        for(let i = 0; i < 4; i++) {
+            const result = await getCounts(arr.slice(i*size, (i+1)*size), key);
+            results += result;
+        }
+        done(results);
+    })();
+}
+
+//module.exports = nmPTP;
